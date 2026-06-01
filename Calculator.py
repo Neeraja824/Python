a606d5a8fbb3365b2b116350e54f1150cc35c20c
Neeraja824 <lankaneeraja824@gmail.com>
2026-06-01 19:59:02 +0530

    files

---
+++ Calculator.py
@@ -0,0 +1,50 @@
+#Python Calculator
+
+operator=input("Enter an operator (+ - * / %): ")
+num1=float(input("Enter the 1st number: "))
+num2=float(input("Enter the second number: "))
+if operator == "+":
+    result=num1+num2
+    print(round(result))
+elif operator == "-":
+    result=num1-num2
+    print(round(result))
+elif operator == "*":
+    result=num1*num2
+    print(round(result))
+elif operator == "/":
+    result=num1/num2
+    print(round(result))
+elif operator == "%":
+    result=num1%num2
+    print(round(result))
+else:
+    print(f"{operator} is not a valid operator!..")
+
+
+#Python weight converter
+weight=float(input("Enter your weight: "))
+unit=input("Kilograms or Pounds? (K or L): ")
+if unit == "K":
+    weight=weight*2.205
+    unit="Lbs."
+    print(f"Your weight is: {round(weight,1)} {unit}")
+elif unit == "L":
+    weight=weight/2.205
+    unit="Kgs."
+    print(f"Your weight is: {round(weight,1)} {unit}")
+else:
+    print(f"{unit} was not valid!..")
+
+
+#Fahrenheit or Celsius
+unit=input("Is this temperature in Celsius or Fahrenheit (C/F): ")
+temp=float(input("Enter the temperature: "))
+if unit == "C":
+    temp=round((9*temp)/5+32,1)
+    print(f"The temperature in Fahrenheit is: {temp} F")
+elif unit == "F":
+    temp=round((temp-32)*5/9,1)
+    print(f"The temperature in Celsius is: {temp} C")
+else:
+    print(f"{unit} is an invalid unit of measurement!..")
--- ConditionalStatements.py
+++ ConditionalStatements.py
@@ -15,3 +15,15 @@ if response=="Y":
 else:
     print("No food for you!")
 
+#For boolean 
+for_sale=True
+if for_sale:
+    print("This item is for sale!")
+else:
+    print("This item is not for sale!")
+
+online=False
+if online:
+    print("The user is online..")
+else:
+    print("The user is offline..")
