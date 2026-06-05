18102049a4d52c850fbe924e7a9696cb13983e63
Neeraja824 <lankaneeraja824@gmail.com>
2026-06-05 12:10:44 +0530

    files

--- ForLoops.py
+++ ForLoops.py
@@ -68,3 +68,82 @@
 #     print(f"{s} is a palindrome..")
 # else:
 #     print(f"{s} is not a palindrome..")
+
+#Sum of even numbers from 1 to N
+# n=int(input("Enter a number: "))
+# sum=0
+# for i in range(1,n+1):
+#     if i%2==0:
+#         sum+=i
+# print(f"The sum of even numbers is: {sum}")
+
+#Sum of odd numbers from 1 to N
+# n=int(input("Enter a number: "))
+# sum=0
+# for i in range(1,n+1):
+#     if i%2!=0:
+#         sum+=i
+# print(f"The sum of even numbers is: {sum}")
+
+#Find the largest digit in a number.
+# a=float(input("Enter a first number: "))
+# b=float(input("Enter a second number: "))
+# c=float(input("Enter a third number: "))
+# max_num=max(a,b,c)
+# print(f"The largest number is: {max_num}")
+
+# Check whether a number is an Armstrong number.
+# n=int(input("Enter a number: "))
+# temp=n
+# digits=len(str(n))
+# sum=0
+# for digit in str(n):
+#     sum+=int(digit)**digits
+# if sum==n:
+#     print(f"{n} is an Armstrong number")
+# else:
+#     print(f"{n} is not an Armstrong number")
+
+#Check whether a number is a Strong number.
+# n = int(input("Enter a number: "))
+# temp = n
+# sum = 0
+# while temp > 0:
+#     digit = temp % 10
+#     fact = 1
+#     for i in range(1, digit + 1):
+#         fact *= i
+#     sum += fact
+#     temp //= 10
+# if sum == n:
+#     print(f"{n} is a Strong Number")
+# else:
+#     print(f"{n} is not a Strong Number")
+
+#Print all factors of a number.
+# n=int(input("Enter a number: "))
+# print(f"Factors of the given number are: ")
+# for i in range(1,n+1):
+#     if n%i==0:
+#         print(i)
+
+#Check whether a number is prime.
+# n=int(input("Enter a number: "))
+# count=0
+# for i in range(1,n+1):
+#     if n%i==0:
+#         count+=1
+# if count==2:
+#     print(f"{n} is a prime number")
+# else:
+#     print(f"{n} is not a prime number")
+
+#Print all prime numbers between 1 and N.
+n = int(input("Enter a number: "))
+for num in range(2, n + 1):
+    count = 0
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count += 1
+    if count == 2:
+        print(num, end=" ")
--- Patterns.py
+++ Patterns.py
@@ -27,3 +27,46 @@
 #     for j in range(i):
 #         print("*",end=" ")
 #     print()    
+
+# n=int(input("Enter a number: "))
+# for i in range(1,n+1):
+#     for j in range(n-i):
+#         print(" ",end=" ")
+#     for k in range(2*i-1):
+#         print("*",end=" ")
+#     print()
+# for i in reversed(range(1,n)):
+#     for j in range(n-i):
+#         print(" ",end=" ")
+#     for k in range(2*i-1):
+#         print("*",end=" ")
+#     print()
+
+# Number Patterns
+
+# n=int(input("Enter a number: "))
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(j+1, end=" ")
+#     print()
+
+# n=int(input("Enter a number: "))
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()
+
+# Alphabet Patterns
+
+# n=int(input("Enter a number: "))
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(chr(64+j+1),end=" ")
+#     print()
+
+# n=int(input("Enter a number: "))
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(chr(64+i),end=" ")
+#     print()
+
