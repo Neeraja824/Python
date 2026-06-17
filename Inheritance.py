55a69282e35a30e940f573379d39252d32e19c8b
Neeraja824 <lankaneeraja824@gmail.com>
2026-06-17 15:05:45 +0530

    files

---
+++ Inheritance.py
@@ -0,0 +1,48 @@
+# Inheritance = Allows a class to inherit attributes and methods from another class
+#               Helps with code reusability and extensibility
+#               class child(parent)
+
+# Single Inheritance
+
+# class Animal:
+#     def __init__(self,name):
+#         self.name=name
+#         self.is_alive=True
+
+#     def eat(self):
+#         print(f"{self.name} is eating")
+    
+#     def sleep(self):
+#         print(f"{self.name} is sleeping")
+
+# class Dog(Animal):
+#     def speak(self):
+#         print("Bow Bow Bowwww!")
+
+# class Cat(Animal):
+#     def speak(self):
+#         print("Meow Meow Meowww!")
+
+# class Mouse(Animal):
+#     def speak(self):
+#         print("Kich Kich Kichhhh!")
+
+# dog=Dog("Leo")
+# cat=Cat("Chiru")
+# mouse=Mouse("Micky")
+
+# print(dog.name)
+# print(dog.is_alive)
+
+# dog.eat()
+# dog.sleep()
+# dog.speak()
+
+# cat.speak()
+# mouse.speak()
+
+# Multiple Inheritance = inherit from more than one parent class
+
+
+
+# Multi-level Inheritance  = inherit from a parent which inherits from another parent
