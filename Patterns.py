c6d9edaaaee7f5bd50289a43de825658dab84d6a
Neeraja824 <lankaneeraja824@gmail.com>
2026-06-30 14:22:09 +0530

    files

--- Patterns.py
+++ Patterns.py
@@ -80,10 +80,19 @@
 
 #Hollow patterns
 
+# n=int(input("Enter a number: "))
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         if j==1 or i==n or j==i:
+#             print("*",end=" ")
+#         else:
+#             print(" ",end=" ")
+#     print()
+
 n=int(input("Enter a number: "))
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        if j==1 or i==n or j==i:
+for i in range(n):
+    for j in range(2*n-1):
+        if(i==0 or i==n-1 or j==0 or j==2*n-2 or i+j==n-1 or i-j==n-1+n):
             print("*",end=" ")
         else:
             print(" ",end=" ")
